5307fc05733dd7f082b3776a1b9be063cd172f54
Alvaro Torres <alvarotorresc@users.noreply.github.com>
2026-08-07 14:22:56 +0200

    fix(compare): alinear y agrandar la cabecera de la tabla
    El <a> era inline, asi que el nombre caia al borde izquierdo de la celda
    mientras el sprite se centraba con sus margenes. Y con el reparto
    automatico las columnas median 504, 319 y 271: nada cuadraba hacia abajo.
    Ahora sprite de 128px, nombre y tipos centrados debajo, y columnas del
    mismo ancho con table-layout fixed.

--- js/compare.js
+++ js/compare.js
@@ -120,7 +120,7 @@ export async function renderCompare(container, query = new URLSearchParams()) {
                   <th>
                     <a href="#/pokedex/${p.id}">
                       <img class="cmp-sprite" src="${spriteUrl(spriteIdFor(p))}" alt="${pokeName(p)}">
-                      <div>${pokeName(p)}</div>
+                      <div class="cmp-name">${pokeName(p)}</div>
                     </a>
                     <div class="cmp-types">${p.types.map(tp => `<span class="type-badge sm" data-type="${tp}">${typeName(tp)}</span>`).join('')}</div>
                   </th>
